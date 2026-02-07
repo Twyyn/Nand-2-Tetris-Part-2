711237d97f9d8d4b05367f45295b029e2441f8e4
Twyyn <jarrettsneed2@gmail.com>
2026-02-07 05:38:23 -0600

    ...

--- vm_translator/src/parser/command.rs
+++ vm_translator/src/parser/command.rs
@@ -1,9 +1,32 @@
+use std::fmt::{self};
+
+#[derive(Debug)]
 pub enum Command {
-    Push{ segment: Segment, index: u64 },
-    Pop,
+    Push { segment: Segment, index: u16 },
+    Pop { segment: Segment, index: u16 },
 }
 
+#[derive(Debug)]
 pub enum Segment {
     Constant,
     Local,
+}
+
+impl fmt::Display for Segment {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Segment::Constant => write!(f, "constant"),
+            Segment::Local => write!(f, "local"),
+        }
+    }
+}
+
+impl fmt::Display for Command {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Command::Push { segment, index } => write!(f, "{} {}", segment, index),
+
+            Command::Pop { segment, index } => write!(f, "{} {}", segment, index),
+        }
+    }
 }
--- vm_translator/src/parser/mod.rs
+++ vm_translator/src/parser/mod.rs
@@ -1,2 +1,17 @@
-#[allow(unused)]
 mod command;
+use command::{Command, Segment};
+
+pub struct Parser {
+    lines: Vec<String>,
+    current: usize,
+}
+
+impl Parser {
+    pub fn new(lines: Vec<String>) -> Self {
+        Self { lines, current: 1 }
+    }
+
+    pub fn parse(&mut self) -> Command {
+        todo!()
+    }
+}
