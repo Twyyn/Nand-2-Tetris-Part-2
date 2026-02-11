3deb9762b3f60de9d342fbbf82da8487b40502fc
Twyyn <131999553+Twyyn@users.noreply.github.com>
2026-02-11 09:50:33 +0000

    ..

--- vm_translator/src/codegen/memory.rs
+++ vm_translator/src/codegen/memory.rs
@@ -10,43 +10,51 @@ impl Memory<'_> {
     pub fn push_to_asm(&self) -> String {
         match self {
             Self::Constant(index) => format!(
-                "@{index}\n\
-                D=A\n\
-                @SP\n\
-                A=M\n\
-                M=D\n\
-                @SP\n\
-                M=M+1\n"
+                "
+                @{index}\n
+                D=A\n
+                @SP\n
+                A=M\n
+                M=D\n
+                @SP\n
+                M=M+1\n
+                "
             ),
             Self::Segment(segment, index) => format!(
-                "@{segment}\n\
-                D=M\n\
-                @{index}\n\
-                A=D+A\n\
-                D=M\n\
-                @SP\n\
-                A=M\n\
-                M=D\n\
-                @SP\n\
-                M=M+1\n"
+                "
+                @{segment}\n
+                D=M\n
+                @{index}\n
+                A=D+A\n
+                D=M\n
+                @SP\n
+                A=M\n
+                M=D\n
+                @SP\n
+                M=M+1\n
+                "
             ),
             Self::Static(filename, index) => format!(
-                "@{filename}.{index}\n\
-                D=M\n\
-                @SP\n\
-                A=M\n\
-                M=D\n\
-                @SP\n\
-                M=M+1\n"
+                "
+                @{filename}.{index}\n
+                D=M\n
+                @SP\n
+                A=M\n
+                M=D\n
+                @SP\n
+                M=M+1\n
+                "
             ),
             Self::Direct(addr) => format!(
-                "@R{addr}\n\
-                D=M\n\
-                @SP\n\
-                A=M\n\
-                M=D\n\
-                @SP\n\
-                M=M+1\n"
+                "
+                @R{addr}\n
+                D=M\n
+                @SP\n
+                A=M\n
+                M=D\n
+                @SP\n
+                M=M+1\n
+                "
             ),
         }
     }
@@ -54,32 +62,38 @@ impl Memory<'_> {
     pub fn pop_to_asm(&self) -> String {
         match self {
             Self::Segment(segment, index) => format!(
-                "@{segment}\n\
-                D=M\n\
-                @{index}\n\
-                D=D+A\n\
-                @R13\n\
-                M=D\n\
-                @SP\n\
-                AM=M-1\n\
-                D=M\n\
-                @R13\n\
-                A=M\n\
-                M=D\n"
+                "
+                @{segment}\n
+                D=M\n
+                @{index}\n
+                D=D+A\n
+                @R13\n
+                M=D\n
+                @SP\n
+                AM=M-1\n
+                D=M\n
+                @R13\n
+                A=M\n
+                M=D\n
+                "
             ),
             Self::Static(filename, index) => format!(
-                "@SP\n\
-                AM=M-1\n\
-                D=M\n\
-                @{filename}.{index}\n\
-                M=D\n"
+                "
+                @SP\n
+                AM=M-1\n
+                D=M\n
+                @{filename}.{index}\n
+                M=D\n
+                "
             ),
             Self::Direct(addr) => format!(
-                "@SP\n\
-                AM=M-1\n\
-                D=M\n\
-                @R{addr}\n\
-                M=D\n"
+                "
+                @SP\n
+                AM=M-1\n
+                D=M\n
+                @R{addr}\n
+                M=D\n
+                "
             ),
             _ => unreachable!(),
         }
